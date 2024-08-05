758be8a1edd06dd3440a90d33c55f99d20e961f1
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-08-05 07:31:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ GIT_TOKEN = getenv(
 )  #❖ Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/roy_editx")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/the_friendz")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/arona_logs")
 
 #❖ Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
